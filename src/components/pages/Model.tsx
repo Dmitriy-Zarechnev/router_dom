512b5da3378f0008c043f7c65fbec75b58ebb050
Dmitriy-Zarechnev <zarechnev5021@gmail.com>
2024-02-21 10:22:09 +0300

    21.02 - useParams + find - 1

--- src/components/pages/Model.tsx
+++ src/components/pages/Model.tsx
@@ -5,20 +5,37 @@ import {SnikersItem} from '../../App'
 
 export const Model = (props: { itemsArr: SnikersItem[] }) => {
     const params = useParams()
-    const actualParams = Number(params.id) - 1
+    // const actualParams = Number(params.id) - 1
+    const currentModel = props.itemsArr.find(el => el.id === Number(params.id))
 
     return (
+        <>
+            {currentModel
+                ? <S.ModelWrapper>
+                    <S.ModelHeader>{currentModel.model}</S.ModelHeader>
+                    <S.SpanText>{currentModel.collection}</S.SpanText>
+                    <S.ModelImg src={currentModel.picture}
+                                alt={`${currentModel.model} img should be here`}/>
+                    <S.SpanText>{currentModel.price}</S.SpanText>
+                </S.ModelWrapper>
+                : <S.ModelHeader>Wait a minute🤡</S.ModelHeader>
+            }
+        </>
+
+        /*
         <>
             {actualParams < props.itemsArr.length
                 ? <S.ModelWrapper>
                     <S.ModelHeader>{props.itemsArr[actualParams].model}</S.ModelHeader>
                     <S.SpanText>{props.itemsArr[actualParams].collection}</S.SpanText>
-                    <S.ModelImg src={props.itemsArr[actualParams].picture}/>
+                    <S.ModelImg src={props.itemsArr[actualParams].picture}
+                                alt={`${props.itemsArr[actualParams].model} img should be here`}/>
                     <S.SpanText>{props.itemsArr[actualParams].price}</S.SpanText>
                 </S.ModelWrapper>
                 : <S.ModelHeader>Wait a minute🤡</S.ModelHeader>
             }
         </>
+        */
     )
 }
 
